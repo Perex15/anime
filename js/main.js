0d74a8c06573e356ceae0e45ada4f81ff10dedca
Perex15 <149716430+Perex15@users.noreply.github.com>
2025-12-23 12:49:36 +0100

    Add 3D hover effect to anime cards

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const cards = document.querySelectorAll(".anime-card");
+
+cards.forEach(card => {
+  card.addEventListener("mousemove", e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const rotateY = ((x / rect.width) - 0.5) * 25;
+    const rotateX = ((y / rect.height) - 0.5) * -25;
+
+    card.style.transform = `
+      rotateY(${rotateY}deg)
+      rotateX(${rotateX}deg)
+      scale(1.06)
+    `;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "rotateY(0deg) rotateX(0deg) scale(1)";
+  });
+});
